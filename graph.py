ca421688bff60f1655e182b76f81082a8a6f45ab
Oscar <oacs1221@gmail.com>
2020-03-08 11:44:42 -0400

    Better doc

--- graph.py
+++ graph.py
@@ -1,19 +1,27 @@
 import cv2
 from PySimpleGUI import Graph, Window
+
 graph = Graph((600, 450), (0, 450), (600, 0), key='GRAPH',
               enable_events=True, drag_submits=True, metadata={
         "a_id": None
     })
 
 
-def graph_events(event, values, window: Window, read):
+def graph_events(event: str, values: dict, window: Window, read) -> None:
+    """
+    Args:
+        event: Event name
+        values: values of the event
+        read: function to read a frame
+        window (Window):
+    """
     frame = read()
     img_bytes = cv2.imencode('.png', frame)[1].tobytes()  # ditto
     if window["GRAPH"].metadata["a_id"]:
         # delete previous image
         window["GRAPH"].delete_figure(window["GRAPH"].metadata["a_id"])
     window["GRAPH"].metadata["a_id"] = window["GRAPH"].draw_image(
-        data=img_bytes, location=(0, 0))    # draw new image
+        data=img_bytes, location=(0, 0))  # draw new image
     # move image to the "bottom" of all other drawings
     window["GRAPH"].TKCanvas.tag_lower(window["GRAPH"].metadata["a_id"])
 
